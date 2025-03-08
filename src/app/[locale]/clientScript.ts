c5db0e599e8268241c7c7a1759c242769d517154
= <=>
2025-03-09 00:25:40 +0400

    init google analytical

---
+++ src/app/[locale]/clientScript.ts
@@ -0,0 +1,25 @@
+"use client"
+ 
+import { useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
+import ReactGA from 'react-ga4'
+
+function ClientScript() {
+
+  // to initialize hotjar services
+  const siteId = 3858905
+  const hotjarVersion = 6
+ 
+  useEffect(() => {
+    // initialize bootstrap 
+    require('bootstrap/dist/js/bootstrap.min.js')
+    // initialize hotjar services
+    // hotjar.initialize(siteId, hotjarVersion)
+    // initialize google services
+    ReactGA.initialize('G-NNTKLKFCD6')
+  }, []);
+
+  return null;
+}
+
+export default ClientScript
